9484fe1f5771d5d4ed04313157a1747b2b1b3c0c
frausto98 <frausto9298@yahoo.com>
2023-07-05 21:56:44 -0700

    added a lot of functionality

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,34 +1,68 @@
 var questions = [
-     {
-     question: "What is the index number for the first item in an array?",
-     answers: [
-         {"1": false},
-         {"100": false},
-         {"0": true},
-         {"5": false},
-     ]
-     },
-     {
-         question: "What is the index number for the first item in an array?",
-         answers: [
-             '1', 9, '2', 7
-         ]
-         },
-]
+    {
+        question: "What is the index number for the first item in an array?",
+        answers: [
+            "1", "100", "0", "5"
+        ],
+        trueAnswer: "0"
+    },
+    {
+        question: "What is the pair name in objects?",
+        answers: [
+            "Apple / Banana", "House / Room", "Key / Value", "Type / Property"
+        ],
+        trueAnswer: "Key / Value"
+    },
+    {
+        question: "Where is local storage?",
+        answers: [
+            "Home", "On the Browser", "The Local Store", "The Computer"
+        ],
+        trueAnswer: "On the Browser"
+    },
+    {
+        question: "What kind of Loop is it?",
+        answers: [
+            "a fruit loop", "a four loop", "4 loops", "For Loop"
+        ],
+        trueAnswer: "For Loop"
+    },
+    {
+        question: "Which of the following allows you to space your syntax?",
+        answers: [
+            "The '1' Key", "Backspace key", "Tab key", "Enter key"
+        ],
+        trueAnswer: "Tab key"
+    }
 
+]
+var highScBtn = document.getElementById('highScoreButton')
 var startBtn = document.getElementById('click')
 var firstContainer = document.getElementById('first')
 var container = document.getElementById('container')
+var summaryPage = document.getElementById('summaryPage')
+var scorePage = document.getElementById('highscores')
+var listedScores = document.getElementById('listedScores')
+var li = document.createElement('li')
 var questionIndex = 0;
-var timerStart = 10;
+var scoreIndex = 0;
+var timerStart = 20;
 var timeText = document.getElementById('timer')
 var clearTime;
+var highScoreArray =  JSON.parse(localStorage.getItem('userInfo')) || []
+
 
 function begin() {
     // hide the container with the button
+    questionIndex = 0;
+    timerStart = 20;
+    timeText.classList.remove('hide');
+    container.classList.remove('hide');
     firstContainer.setAttribute('class', 'hide')
+    summaryPage.setAttribute('class', 'hide')
+    scorePage.setAttribute('class', 'hide')
     // display the container with the id container
-    container.classList.remove('hide')
+    
 
     // start the timer and display the time on the page
     timeText.textContent = 'time: ' + timerStart
@@ -43,11 +77,11 @@ function begin() {
     }, 1000);
 
 
-    displayNames()
+    displayQuestions()
 
 }
 
-function displayNames() {
+function displayQuestions() {
     // empty the container before creating the next items
     container.innerHTML = ''
 
@@ -68,7 +102,7 @@ function displayNames() {
     // container.append(nextBtn)
 
     for (var i = 0; i < questions[questionIndex].answers.length; i++) {
-        var nextBtn =  document.createElement('button')
+        var nextBtn = document.createElement('button')
         nextBtn.textContent = questions[questionIndex].answers[i]
 
         nextBtn.addEventListener('click', nextItem)
@@ -79,16 +113,119 @@ function displayNames() {
 }
 
 function nextItem() {
+    //checks answers
+    //if right, display correct
+    //if wrong, remove time, and display wrong
+    
     questionIndex++;
-    if(questions.length === questionIndex){
-      gameOver()
-    }else{
-    displayNames();
+    if (questions.length === questionIndex) {
+        gameOver()
+    } else {
+        displayQuestions();
     }
 }
 
-function gameOver(){
-    clearInterval(clearTime)
+
+
+function gameOver() {
+    summaryPage.innerHTML = '';
+    clearInterval(clearTime);
+    //questions clear
+    container.setAttribute('class', 'hide');
+
+    summaryPage.classList.remove('hide');
+    //message appears saying all done
+    var h1El = document.createElement('h1');
+    h1El.textContent = "Game Over";
+
+    // create element to display score
+    var timeScore = document.createElement('h3');
+    timeScore.textContent = "High Score: " + timerStart
+
+    //input field for intials + display remaining time
+        // <div>
+        //  <label for='input'>Initials:</label>
+        //  <input type='text' id='input' />
+        //  <button type='button' id='saveBtn'>Submit</button>
+        // </div>
+    var div = document.createElement('div')
+    var label = document.createElement('label')
+    var input = document.createElement('input')
+    var button = document.createElement('button')
+
+    label.setAttribute('for', 'input')
+    label.textContent = 'Initials:'
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'input')
+    button.setAttribute('type', 'button')
+    button.setAttribute('id', 'saveBtn')
+    button.textContent = "Save Here";
+    
+    //added event listener to start a function once Save Here us clicked
+button.addEventListener('click', saveFiles)
+
+    //appends the label, input and button to the divided content on the page.
+div.append(label, input, button);
+    //appends the h1
+summaryPage.append(h1El, timeScore, div)
+
+}
+
+function saveFiles() {
+    var input = document.getElementById('input')
+    var userScore = { 
+        userInit: input.value,
+        highScore: timerStart,
+    }
+highScoreArray.push(userScore)
+localStorage.setItem("userInfo", JSON.stringify(highScoreArray))
+showScores()
+}
+
+function showScores(){
+    scorePage.innerHTML = '';
+    firstContainer.setAttribute('class', 'hide')
+    summaryPage.setAttribute('class', 'hide');
+    timeText.setAttribute('class', 'hide');
+    scorePage.classList.remove('hide');
+    var button1 = document.createElement('button');
+    var button2 = document.createElement('button');
+    button1.setAttribute('type', 'button');
+    button1.setAttribute('id', 'startOver')
+    button2.setAttribute('type', 'button')
+    button2.setAttribute('id', 'clearScoreButton')
+    button1.textContent = "Start Over";
+    button2.textContent = "Clear Score";
+    var h1El = document.createElement('h1')
+    h1El.textContent = "High Scores: "
+
+    scorePage.append(button1, button2, h1El);
+    button1.addEventListener('click', begin)
+    button2.addEventListener('click', clearScores)
+
+    for (var i = 0; i < highScoreArray.length; i++) {
+        var listed = highScoreArray;
+        li;
+        li.textContent = listed
+
+        scorePage.append(li)
+        
+        
+        
+    //scoreboard w "start over" & "clear score"
+    
+    // create the list of highsscores
+    }
+
+    
+}
+
+function clearScores () {
+    localStorage.clear();
+    remove = document.removeChild(li);
+    remove
 }
 
 startBtn.addEventListener('click', begin)
+highScBtn.addEventListener('click', showScores)
+
